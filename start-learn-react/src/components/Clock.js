0c3684db5df24cfce6a4e4981ac8680d49292ecf
user <ujjalroy7862@gmail.com>
2023-03-06 02:29:57 +0600

    done the react component

--- start-learn-react/src/components/Clock.js
+++ start-learn-react/src/components/Clock.js
@@ -1,18 +1,24 @@
 import React from "react";
 
 class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date() };
-  }
+  state = { date: new Date() };
 
   componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        date: new Date(),
-      });
+    this.clockTimer = setInterval(() => {
+      this.tick();
     }, 1000);
   }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.clockTimer);
+  }
+
   render() {
     return (
       <div>
