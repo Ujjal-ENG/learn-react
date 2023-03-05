1c3df891838730b1093e684619083f162c34e324
user <ujjalroy7862@gmail.com>
2023-03-06 03:13:05 +0600

    right now done but i can not fix the eslint error

--- start-learn-react/src/App.js
+++ start-learn-react/src/App.js
@@ -1,7 +1,7 @@
 import Clock from "./components/Clock";
 
 function App() {
-  return <Clock locale="bn-BD">Mama Kopa Japiya</Clock>;
+  return <Clock locale="bn-BD">Mama Kotr Japiya</Clock>;
 }
 
 export default App;
